04ade9f14b394e5d8fd27e798b093e613921d88e
rakin406 <rakinhacker@gmail.com>
2021-05-13 13:40:20 +0600

    get user input

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,39 @@ use termion::color;
 
 fn main() {
     let mut color = "blue";
-    let text = "This is a rainbow! Isn't it beautiful? I am still working on improving it :)";
+
+    print!("Enter text: ");
+    io::stdout().flush().unwrap();
+
+    // User input
+    let mut text = String::new();
+    io::stdin()
+        .read_line(&mut text)
+        .ok()
+        .expect("Couldn't read line");
 
     loop {
         for c in text.chars() {
-            // Rainbow!
-            if color == "blue" {
-                print!("{}{}", color::Fg(color::Blue), c);
-                color = "green";
-            } else if color == "green" {
-                print!("{}{}", color::Fg(color::Green), c);
-                color = "magenta";
-            } else if color == "magenta" {
-                print!("{}{}", color::Fg(color::Magenta), c);
-                color = "red";
-            } else if color == "red" {
-                print!("{}{}", color::Fg(color::Red), c);
-                color = "yellow";
-            } else if color == "yellow" {
-                print!("{}{}", color::Fg(color::Yellow), c);
-                color = "blue";
+            if c == ' ' {
+                print!(" ");
+            } else {
+                // Rainbow!
+                if color == "blue" {
+                    print!("{}{}", color::Fg(color::Blue), c);
+                    color = "green";
+                } else if color == "green" {
+                    print!("{}{}", color::Fg(color::Green), c);
+                    color = "magenta";
+                } else if color == "magenta" {
+                    print!("{}{}", color::Fg(color::Magenta), c);
+                    color = "red";
+                } else if color == "red" {
+                    print!("{}{}", color::Fg(color::Red), c);
+                    color = "yellow";
+                } else if color == "yellow" {
+                    print!("{}{}", color::Fg(color::Yellow), c);
+                    color = "blue";
+                }
             }
 
             io::stdout().flush().unwrap();
