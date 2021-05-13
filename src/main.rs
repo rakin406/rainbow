3b6a1757163312d65a54eb19bbccced15e912722
rakin406 <rakinhacker@gmail.com>
2021-05-13 13:21:50 +0600

    Continously change color

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,30 @@ use termion::color;
 
 fn main() {
     let mut color = "blue";
-    let text = "Text";
+    let text = "This is a rainbow! Isn't it beautiful? I am still working on improving it :)";
 
-    for c in text.chars() {
-        // Rainbow!
-        if color == "blue" {
-            print!("{}{}", color::Fg(color::Blue), c);
-            color = "green";
-        } else if color == "green" {
-            print!("{}{}", color::Fg(color::Green), c);
-            color = "magenta";
-        } else if color == "magenta" {
-            print!("{}{}", color::Fg(color::Magenta), c);
-            color = "red";
-        } else if color == "red" {
-            print!("{}{}", color::Fg(color::Red), c);
-            color = "yellow";
-        } else if color == "yellow" {
-            print!("{}{}", color::Fg(color::Yellow), c);
-            color = "blue";
+    loop {
+        for c in text.chars() {
+            // Rainbow!
+            if color == "blue" {
+                print!("{}{}", color::Fg(color::Blue), c);
+                color = "green";
+            } else if color == "green" {
+                print!("{}{}", color::Fg(color::Green), c);
+                color = "magenta";
+            } else if color == "magenta" {
+                print!("{}{}", color::Fg(color::Magenta), c);
+                color = "red";
+            } else if color == "red" {
+                print!("{}{}", color::Fg(color::Red), c);
+                color = "yellow";
+            } else if color == "yellow" {
+                print!("{}{}", color::Fg(color::Yellow), c);
+                color = "blue";
+            }
+
+            io::stdout().flush().unwrap();
         }
-
-        io::stdout().flush().unwrap();
+        print!("\x1B[2J\x1B[1;1H"); // clear terminal screen
     }
 }
