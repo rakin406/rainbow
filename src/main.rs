2f2406f35b6202af36e7886f39ecbb7bc234f34e
rakin406 <rakinhacker@gmail.com>
2021-05-13 13:14:24 +0600

    rainbow output

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 extern crate termion;
 
+use std::io::{self, Write};
 use termion::color;
 
 fn main() {
-    println!("{}Red", color::Fg(color::Red));
-    println!("{}Blue", color::Fg(color::Blue));
+    let mut color = "blue";
+    let text = "Text";
+
+    for c in text.chars() {
+        // Rainbow!
+        if color == "blue" {
+            print!("{}{}", color::Fg(color::Blue), c);
+            color = "green";
+        } else if color == "green" {
+            print!("{}{}", color::Fg(color::Green), c);
+            color = "magenta";
+        } else if color == "magenta" {
+            print!("{}{}", color::Fg(color::Magenta), c);
+            color = "red";
+        } else if color == "red" {
+            print!("{}{}", color::Fg(color::Red), c);
+            color = "yellow";
+        } else if color == "yellow" {
+            print!("{}{}", color::Fg(color::Yellow), c);
+            color = "blue";
+        }
+
+        io::stdout().flush().unwrap();
+    }
 }
